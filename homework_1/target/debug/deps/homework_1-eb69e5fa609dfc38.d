563a13f698d7436343abd0cba5e4d0f9838f0e1a
AndreyTorkhov <124518996+AndreyTorkhov@users.noreply.github.com>
2023-06-29 19:53:30 +0300

    Add files via upload

---
+++ homework_1/target/debug/deps/homework_1-eb69e5fa609dfc38.d
@@ -0,0 +1,5 @@
+/home/andre_root/homework_1/target/debug/deps/homework_1-eb69e5fa609dfc38: src/main.rs
+
+/home/andre_root/homework_1/target/debug/deps/homework_1-eb69e5fa609dfc38.d: src/main.rs
+
+src/main.rs:
---
+++ homework_1/target/debug/homework_1.d
@@ -0,0 +1,1 @@
+/home/andre_root/homework_1/target/debug/homework_1: /home/andre_root/homework_1/src/main.rs
